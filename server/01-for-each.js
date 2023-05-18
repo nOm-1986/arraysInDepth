7949408aab3872eb04e32313b2bd4ec46b801c04
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-18 17:01:06 -0500

    forEach instead for

---
+++ server/01-for-each.js
@@ -0,0 +1,10 @@
+const letters = ['a', 'b', 'c'];
+
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    console.log(element);
+}
+console.log('===================')
+//Reducing with arrays methods.
+letters.forEach(el => console.log(el));
+
