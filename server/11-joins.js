dc78d91db691565f457671170d95c50045a5a16d
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-29 11:29:53 -0500

    Joins - easy peasy leamon squeeze

---
+++ server/11-joins.js
@@ -0,0 +1,10 @@
+const elements = ['fire', 'Air', 'Water', 'Earth'];
+
+const rta = elements.join('--');
+console.log('rta', rta);
+
+//Example 2.
+const title = 'Curso de manipulación de arrays';
+const splited = title.split(' ');
+const urlFinal = splited.join('-').toLowerCase();
+console.log(urlFinal);
