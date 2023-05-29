12d65c155f20ba18d20d475a65ec8f94281f6158
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-29 12:20:39 -0500

    Using FlatMap, amazing

---
+++ server/14-flatMap.js
@@ -0,0 +1,46 @@
+const users = [
+    { userid: 1, username: "Tom", attributes: ['Nice','Cute']},
+    { userid: 2, username: "Mike", attributes: ['Lovely']},
+    { userid: 3, username: "Nico", attributes: ['Nice','Cool']}
+];
+
+const rta = users.map(item => item.attributes).flat();
+console.log(rta);
+
+console.log('________________________')
+const rta2 = users.flatMap(user => user.attributes);
+console.log('rta2', rta2);
+
+
+const calendars = {
+    primaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita 1",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 17),
+        endDate: new Date(2021, 1, 1, 18),
+        title: "Cita 2",
+      },
+    ],
+    secondaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 12),
+        endDate: new Date(2021, 1, 1, 12, 30),
+        title: "Cita 2",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 9),
+        endDate: new Date(2021, 1, 1, 10),
+        title: "Cita 4",
+      },
+    ],
+  };
+
+const challenge = calendars.primaryCalendar.flatMap(item => item.startDate);
+//console.log(challenge);
+
+const rta3 = Object.values(calendars).flatMap(item => item.map(date => date.startDate));
+console.log(rta3);
