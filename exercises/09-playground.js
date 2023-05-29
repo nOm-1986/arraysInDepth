966977ee5482dcbb8cbc8f6b01fe8004af481177
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-29 12:49:08 -0500

    playground 9 solved

---
+++ exercises/09-playground.js
@@ -0,0 +1,11 @@
+const strings = [
+    "Beautiful is better than ugly",
+    "Explicit is better than implicit",
+    "Simple is better than complex",
+    "Complex is better than complicated",
+];
+
+const newA = [];
+
+const challenge = strings.join(' ').split(' ').length;
+console.log(challenge);
