758cc5b43808b843d3a1dfb404559b65512a034c
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-06-06 09:31:39 -0500

    Flatting an array, easy

---
+++ exercises/ArraysPro/02-exercise.js
@@ -0,0 +1,32 @@
+/*
+En este desafío tienes un array que contiene otros arrays internos, tu reto es retornar un array que tenga 
+los elementos de los otros arrays eliminando los arrays internos y dejando solo los valores, 
+normalmente a esto le llamamos aplanar el array o "Flattening Algorithm".
+
+Para solucionarlo vas a encontrar una función llamada flatArray que recibe un parámetro de entrada:
+
+array: Un array que tiene otros arrays dentro
+Dentro del cuerpo de la función flatArray debes escribir tu solución.
+
+Ejemplo:
+
+Input:
+[
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+
+Output:
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
+const input = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+const flatArray = (arr) => {
+    return arr.flat();
+}
+console.log(flatArray(input));
